9c07ff71772f8dae54462d448a010fb16704ac8d
Sparsh394 <agarwalla.sparsh@gmail.com>
2020-11-23 19:23:03 +0530

    Update justwatch_scraper.py

--- justwatch_scraper.py
+++ justwatch_scraper.py
@@ -9,9 +9,10 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 import time
+import os
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+# GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 def get_links(movie): 
     movie=re.sub(r'([^\s\w]|_)+', '', movie.lower().strip())
@@ -23,9 +24,10 @@ def get_links(movie):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    chrome_options.add_argument('--diable-dev-shm-usage')
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.set_window_size(1920, 1080)
     driver.maximize_window() 
     driver.get(url)
